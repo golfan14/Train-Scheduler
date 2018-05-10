cfd38a310f89daf1e7f251f8f9f97c0bd8409e31
Stephen Ermisch <stephenermisch@Stephens-MacBook-Pro.local>
2018-05-10 00:04:40 -0600

    table-data

--- logic.js
+++ logic.js
@@ -24,6 +24,10 @@ $("#submit-button").on("click", function (event) {
     firstTime = $("#time-input").val().trim();
     frequency = $("#freq-input").val().trim();
 
+    if (name == "Hogwarts Express") {
+        alert("NERD");
+    }
+
     console.log(name);
     console.log(destination);
     console.log(firstTime);
@@ -34,6 +38,44 @@ $("#submit-button").on("click", function (event) {
         destination: destination,
         firstTime: firstTime,
         frequency: frequency
-    });
+    });    
+
+    $("#name-input").val("");
+    $("#dest-input").val("");
+    $("#time-input").val("");
+    $("#freq-input").val("");
 });
+
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val().name);
+    console.log(childSnapshot.val().destination);
+    console.log(childSnapshot.val().firstTime);
+    console.log(childSnapshot.val().frequency);  
+
+    $("#table-data").append("<tr><td>" + childSnapshot.val().name + "</td>" + "<td>" + childSnapshot.val().destination + "</td>" + "<td>" + childSnapshot.val().frequency + "</td>" + "<td>" + nextArrival + "</td>" + "<td>" + tMinutesTillTrain + "</td></tr>");
+});
+
+var firstTimeConverted = moment(firstTime, "hh:mm");
+console.log(firstTimeConverted);
+
+var currentTime = moment();
+console.log(moment(currentTime).format("hh:mm"));
+
+var diffTime = moment().diff(moment(firstTime, "minutes"));
+console.log(diffTime);
+
+var tRemainder = diffTime % frequency;
+console.log(tRemainder);
+
+var tMinutesTillTrain = frequency - tRemainder;
+console.log(tMinutesTillTrain);
+
+var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+console.log(moment(nextTrain).format("hh:mm"));
+
+var nextArrival = moment.unix(nextTrain).format("hh:mm");
+
+
+
+
 
